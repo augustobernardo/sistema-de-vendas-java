f8232514782a206c6fc972fe095abfcdb422ce49
Lincoln Meirelles <lincoln.meirelles@icloud.com>
2023-04-23 03:02:55 -0300

    Nome das variáveis e alguns tratamentos de erros

--- SistemaVendas/src/main/java/br/unisales/alunos/sistemavendas/Persistence/CRUD.java
+++ SistemaVendas/src/main/java/br/unisales/alunos/sistemavendas/Persistence/CRUD.java
@@ -16,149 +16,151 @@ import org.hibernate.Session;
  * @author enrico.eu
  */
 public class CRUD {
-    Session s = HibernateUtil.getInstance();
-    Scanner sc = new Scanner(System.in);
+    Session session = HibernateUtil.getInstance();
+    Scanner scanner = new Scanner(System.in);
 
     public void insert() {
-        EntityManager em = HibernateUtil.getInstance(); // recupera um gerenciador de entidades
+        EntityManager em = HibernateUtil.getInstance();
         try {
             System.out.println("Digite o nome do cliente:");
-            String nomeCliente = sc.nextLine();
+            String nomeCliente = scanner.nextLine();
 
             System.out.println("Digite o nome do produto:");
-            String nomeProduto = sc.nextLine();
+            String nomeProduto = scanner.nextLine();
 
             System.out.println("Preço do " + nomeProduto + " ?");
-            String preco = sc.next();
+            String preco = scanner.next();
             double precoProduto = Double.parseDouble(preco.replace(",", "."));
 
             System.out.println("Quantos " + nomeProduto + " foram vendidos:");
-            int quantidadeVendida = sc.nextInt();
+            int quantidadeVendida = scanner.nextInt();
 
-            em.getTransaction().begin(); // inicializa a transação
-            Venda v = new Venda(nomeCliente, nomeProduto, quantidadeVendida, precoProduto); // cria uma entidade
+            em.getTransaction().begin();
+            Venda v = new Venda(nomeCliente, nomeProduto, quantidadeVendida, precoProduto);
 
-            em.persist(v); // insere no banco
-            em.getTransaction().commit(); // comita a transação
-
+            em.persist(v);
+            em.getTransaction().commit();
             System.out.println("""
                                \n------------------------------
                                Venda adicionada com sucesso
                                ------------------------------\n""");
 
         } catch (Exception ex) {
-            em.getTransaction().rollback(); // invalida a transação
+            em.getTransaction().rollback();
             System.out.println("Erro ao adicionar no banco de dados: " + ex.getLocalizedMessage());
         }
-        em.close(); // fecha o gerenciador
+        em.close();
     }
 
     public void select() {
-        //lista pessoas usando query JPQL
-        List<Venda> listaVenda = s.createQuery("select V from Venda V order by V.nomeCliente", Venda.class).getResultList();
-        //percorre a lista de pesoas
-        for (Venda vl : listaVenda) {
-            System.out.println("(Id: " + vl.getId() + ") " + vl.getNomeProduto() + " vendido para " + vl.getNomeCliente() + " pelo total de: R$" + vl.getTotalVenda());
-        }
+        List<Venda> consultaVendas = session.createQuery("select V from Venda V", Venda.class).getResultList();
+        consultaVendas.forEach(venda -> {
+            System.out.println("Id: " + venda.getId() + " | " + venda.getNomeProduto() + " vendido para " + venda.getNomeCliente() + " pelo total de: R$" + venda.getTotalVenda());
+        });
     }
 
     public void update() {
         System.out.println("Digite o id da venda:");
-        Long id = sc.nextLong();
-        Venda venda = s.find(Venda.class, id);
+        Long id = scanner.nextLong(); 
+        Venda venda = session.find(Venda.class, id);
+        if (venda != null){
+            int optUpdate = 0; 
+            do {
+                String label = "";
 
-        int optUpdate = 0;
+                if (optUpdate > 0) {
+                    label = "\nDeseja alterar mais algo?\n";
 
-        do {
-            String label = "";
+                } else {
+                    label = "\nO que deseja alterar na venda?\n";
+                } 
 
-            if (optUpdate > 0) {
-                label = "\nDeseja alterar mais algo?";
+                System.out.println(label + """ 
+                                   1 - Nome do cliente
+                                   2 - Nome do produto
+                                   3 - Preço do produto
+                                   4 - Quantidade vendida do produto 
+                                   0 - Ir para o menu inicial""");
+                optUpdate = scanner.nextInt();
+                try {
+                    session.getTransaction().begin();
 
-            } else {
-                label = "\nO que deseja alterar na venda?";
-            }
-
-            System.out.println(label
-                    + "\n1 - Nome do cliente"
-                    + "\n2 - Nome do produto"
-                    + "\n3 - Preço do produto"
-                    + "\n4 - Quantidade vendida do produto"
-                    + "\n0 - Ir para o menu inicial");
-            optUpdate = sc.nextInt();
-
-            s.getTransaction().begin();  //inicia a transação
-
-            switch (optUpdate) {
-                case 1 -> {
-                    System.out.println("Digite o novo nome do cliente:");
-                    String nomeClienteUpdate = sc.nextLine();
-
-                    venda.setNomeCliente(nomeClienteUpdate);
-                }
-                case 2 -> {
-                    System.out.println("Digite o nome do produto para o qual deseja alterar:");
-                    String nomeProdutoUpdate = sc.nextLine();
-
-                    venda.setNomeProduto(nomeProdutoUpdate);
-                }
-                case 3 -> {
-                    System.out.println("Novo preço para " + venda.getPrecoProduto() + ": ");
-                    String p = sc.next();
-                    double precoProdutoUpdate = Double.parseDouble(p.replace(",", "."));
-
-                    venda.setPrecoProduto(precoProdutoUpdate);
-                }
-                case 4 -> {
-                    System.out.println("Para qual quantidade vendida deseja alterar?");
-                    int quantidadeVendidaUpdate = sc.nextInt();
-
-                    venda.setQuantidadeVendida(quantidadeVendidaUpdate);
-                }
-            }
-
-            s.persist(venda);
-            s.getTransaction().commit();//confirma a transação
-            System.out.println("""
-                               \n------------------------------
-                               Venda atualizada com sucesso
-                               ------------------------------\n""");
-        } while (optUpdate != 0);
+                switch (optUpdate) {
+                    case 1 -> {
+                        System.out.println("Digite o novo nome do cliente:");
+                        String nomeCliente = scanner.nextLine(); 
+                        venda.setNomeCliente(nomeCliente);
+                    }
+                    case 2 -> {
+                        System.out.println("Digite o nome do produto para o qual deseja alterar:");
+                        String nomeProduto = scanner.nextLine(); 
+                        venda.setNomeProduto(nomeProduto);
+                    }
+                    case 3 -> {
+                        System.out.println("Novo preço para " + venda.getPrecoProduto() + ": ");
+                        String p = scanner.next();
+                        double precoProduto = Double.parseDouble(p.replace(",", ".")); 
+                        venda.setPrecoProduto(precoProduto);
+                    }
+                    case 4 -> {
+                        System.out.println("Para qual quantidade vendida deseja alterar?");
+                        int quantidadeVendida = scanner.nextInt(); 
+                        venda.setQuantidadeVendida(quantidadeVendida);
+                    } 
+                } 
+                session.persist(venda);
+                session.getTransaction().commit();
+                System.out.println("""
+                                   ------------------------------
+                                    Venda atualizada com sucesso
+                                   ------------------------------""");
+                } catch (Exception e) {
+                    session.getTransaction().rollback();
+                    System.out.println("""
+                                ------------------------------
+                                    Erro ao alterar venda
+                                ------------------------------""");
+                    System.out.println(e.getLocalizedMessage()); 
+                } 
+            } while (optUpdate != 0);
+        } else System.out.println("\nVenda não encontrada!\n");
     }
 
     public void delete() {
-        s.getTransaction().begin();//inicia a transação
+        session.getTransaction().begin();
         System.out.println("Digite o id da venda");
-        Long ide = sc.nextLong();
-        //seleciona uma pessoa pelo id
-        //indicada a classe para pesquisa - Pessoa.class
-        Venda vd = s.find(Venda.class, ide);
-        s.remove(vd);//exclui no banco
-        s.getTransaction().commit();//confirma a transação
-        
-        System.out.println("""
+        try {
+            Long idVenda = scanner.nextLong();
+            Venda venda = session.find(Venda.class, idVenda);
+            session.remove(venda);
+            session.getTransaction().commit(); 
+            System.out.println("""
+                                   \n------------------------------
+                                        Venda removida com sucesso
+                                   ------------------------------\n""");
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            System.out.println("""
                                \n------------------------------
-                               Venda removida com sucesso
+                                    Erro ao excluir venda
                                ------------------------------\n""");
+            System.out.println(e.getLocalizedMessage()); 
+        }
+       
     }
 
     public void search() {
         System.out.println("Digite o nome do cliente para listar as vendas efetuadas para o mesmo:");
-        String inicio = sc.nextLine();
-        //cria uma Query com parâmetro
-        //observe que o parâmetro é iniciado com dois pontos ':'
-        Query q = s.createQuery("select V from Venda V where V.nomeCliente like :nome order by V.nomeProduto", Venda.class);
-        //indica nominalmente o valor do parâmetro antes da execução da instrução SQL
-        q.setParameter("nome", inicio + "%");
-        //executa a instrução SQL
+        String nomeCliente = scanner.nextLine();
+        Query q = session.createQuery("select V from Venda V where V.nomeCliente like :NOME order by V.id", Venda.class);
+        q.setParameter("NOME", nomeCliente + "%");
         List<Venda> listaVendas = q.getResultList();
-        //percorre a lista de pessoas
         System.out.println("Abaixo encontra-se a lista de produtos vendidos para " + listaVendas.get(0).getNomeCliente() + " :");
-        for (Venda vl : listaVendas) {
-            System.out.println("Id(" + vl.getId() + ") - "
-                    + vl.getNomeProduto()
-                    + "(quantidade: " + vl.getQuantidadeVendida() + ") "
-                    + " - Total da compra: R$" + vl.getTotalVenda());
+        for (Venda venda : listaVendas) {
+            System.out.println("Id(" + venda.getId() + ") - "
+                    + venda.getNomeProduto()
+                    + "(quantidade: " + venda.getQuantidadeVendida() + ") "
+                    + " - Total da compra: R$" + venda.getTotalVenda());
         }
     }
 
--- SistemaVendas/src/main/java/br/unisales/alunos/sistemavendas/SistemaVendas.java
+++ SistemaVendas/src/main/java/br/unisales/alunos/sistemavendas/SistemaVendas.java
@@ -9,12 +9,10 @@ import java.util.Scanner;
 public class SistemaVendas {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in); 
         CRUD operacoes = new CRUD();
         int op = 0;
-
-        //menu de execução
+ 
         do {
             System.out.println("""
                                Sistema de Registro de Vendas
@@ -24,12 +22,10 @@ public class SistemaVendas {
                                4 - Excluir venda
                                5 - Pesquisar venda por nome do cliente
                                0 - sair""");
-            op = sc.nextInt();
+            op = scanner.nextInt();
             switch (op) {
-                case 1 -> operacoes.insert();
-
-                case 2 -> operacoes.select();
-
+                case 1 -> operacoes.insert(); 
+                case 2 -> operacoes.select(); 
                 case 3 -> operacoes.update();
                 case 4 -> operacoes.delete();
                 case 5 -> operacoes.search();
